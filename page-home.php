d267d0e0304c257713c2eb13b31cc8494d3b85d8
JulianoPastana <julianopastanadg@gmail.com>
2025-07-17 23:48:41 -0300

    Criação Page-home

---
+++ page-home.php
@@ -0,0 +1,216 @@
+    <?php get_header(); ?>
+
+    <section class="s-hero">
+        <div class="container">
+            <div class="left" data-aos="fade-right">    
+                <h3> Abra sua conta, é só baixar o app!</h3>
+                <h1>Desperte seu potencial financeiro com o Bankboost!</h1>
+                <a href="#" class="btn">Abra sua conta digital</a>
+                <ul>
+                    <li>
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-anuidade.svg" alt="Icone de cartão sem anuidade">
+                        <span>Cartão sem <br>anuidade</span>
+                    </li>
+                    <li>
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-conta-digital.svg" alt="Icone de conta digital">
+                        <span>Conta digital <br>100% grátis</span>
+                    </li>
+                    <li>
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-dinheiro.svg" alt="Icone de dinheiro rendendo">
+                        <span>Seu dinheiro <br>rendendo mais</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="right" data-aos="fade-left">
+                <h2>Banco 100% digital</h2>
+                <div class="cards">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/card-hero-01.png" class="item card-one" alt="">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/card-hero-02.png" class=" item card-two" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="s-boost">
+        <div class="container">
+            <div class="left" data-aos="fade-up">
+                <div class="mockup">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/mockup-iphone-01.svg" alt="mockup com funcionalidades do app Bankboost">
+                </div>
+                <div class="description">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/icon-app-boost.svg" class="icon" alt="">
+                    <h3>Baixe nosso app</h3>
+                    <p>Que tal abrir uma conta digital para ver como a gente faz a sua vida muito mais simples?</p>
+                    <ul>
+                        <li>
+                            <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/button - ios.svg" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/button - android.svg" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right" data-aos="fade-left">
+                <h2>Resolva sua vida direto pelo app boost!</h2>
+                <ul class="list">
+                    <li>
+                        <div class="icon">
+                            <img src="<?php echo get_template_directory_uri() ?>/img/icon-credito.svg" alt="icone de cartao de crédito">
+                        </div>
+                        <div class="text">
+                            <h4>Cartão de crédito sem anuidade</h4>
+                            <p>Conta digital com cartão de crédito sem anuidade e sem complicação</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon">
+                            <img src="<?php echo get_template_directory_uri() ?>/img/icon-taxas.svg" alt="icone de sem taxas">
+                        </div>
+                        <div class="text">
+                            <h4>Sem taxas</h4>
+                            <p>Transferências, boletos de depósito e outros serviços gratuitos</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon">
+                            <img src="<?php echo get_template_directory_uri() ?>/img/icon-investimentos.svg" alt="icone de investimentos">
+                        </div>
+                        <div class="text">
+                            <h4>Mais investimentos</h4>
+                            <p>Rendem mais que a poupança e você resgata quando quiser</p>
+                        </div>
+                    </li>
+                </ul>
+                <a href="#" class="btn">Conheça outros produtos</a>
+
+                <div class="box">
+                    <div class="text">
+                        <h3>Boost Pejota</h3>
+                        <h5>Contas digitais PJ gratuitas para decolar seu negócio!</h5>
+                        <p>As melhores contas para fazer pagamentos, compras e receber dos seus clientes.</p>
+                        <ul>
+                            <li>
+                                <button>Sou MEI</button>
+                            </li>
+                            <li>
+                                <button>Sou ME</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri() ?>/img/card-front-pj.svg" class="card" alt="">
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="s-depositions">
+        <div class="container" data-aos="fade-down">
+            <div class="title">
+                <h2>Prático, fácil, moderno
+                    <strong>Você resolve tudo sem estresse 😊</strong>
+                </h2>
+                <div class="swiper-pagination"></div>
+            </div>
+            
+            <div class="slide-depositions">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="card-deposition">
+                            <div class="user">
+                                <span>@marciogurka</span>
+                                <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="icone do twitter antigo">
+                            </div>
+                            <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bankBoost</p>
+                        </div>  
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="card-deposition">
+                            <div class="user">
+                                <span>@kleriene18</span>
+                                <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="icone do twitter antigo">
+                            </div>
+                            <p>Eu uso o Bankboost pra render o pouquinho que eu tenho. Como disse minha amiga Bruna, um dia eu abro o app de novo e PA: tô rica.</p>
+                        </div>  
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="card-deposition">
+                            <div class="user">
+                                <span>@mzltweet</span>
+                                <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="icone do twitter antigo">
+                            </div>
+                            <p> Só por ter um cartão virtual com bloqueio fácil no app, já ganha e muito na segurançã pro cliente.</p>
+                        </div>  
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="card-deposition">
+                            <div class="user">
+                                <span>@marciogurka</span>
+                                <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="icone do twitter antigo">
+                            </div>
+                            <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bankBoost</p>
+                        </div>  
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="card-deposition">
+                            <div class="user">
+                                <span>@marciogurka</span>
+                                <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="icone do twitter antigo">
+                            </div>
+                            <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bankBoost</p>
+                        </div>  
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="card-deposition">
+                            <div class="user">
+                                <span>@marciogurka</span>
+                                <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="icone do twitter antigo">
+                            </div>
+                            <p>Valeu @banconenon! Linda embalagem e o cartão tbm é lindo! Muito amor por esse banco #bankBoost</p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="s-account">
+        <div class="container">
+            <div class="left" data-aos="fade-right">
+                <h2>Aproveite
+                    <strong>Domine o seu dinheiro com uma conta 100% digital</strong>
+                </h2>
+                <ul>
+                    <li>
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-card.svg" alt="icone de cartao">
+                        <div class="text">
+                            <h4>Cartão visa internacional</h4>
+                            <p>Tenha facilidades e benefícios para o seu dia a dia.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-zero.svg" alt="icone de cartao">
+                        <div class="text">
+                            <h4>Zero mensalidade e anuidade</h4>
+                            <p>Não gaste grana pagando taxas desnecessárias.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-investir.svg" alt="icone de cartao">
+                        <div class="text">
+                            <h4>Investimento que rende mais que a poupança</h4>
+                            <p>Invista o seu dinheiro de maneira mais rentável.</p>
+                        </div>
+                    </li>
+                </ul>
+                <a href="#" class="btn">Abra sua conta digital</a>
+            </div>
+            <div class="right">
+                <img src="<?php echo get_template_directory_uri() ?>/img/mockup-app-01.png"  class="mockup one" data-aos="fade-up" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/img/mockup-app-02.png"  class="mockup two" data-aos="fade-down" alt="">
+            </div>
+        </div>
+    </section>
+
+    <?php get_footer(); ?> 
